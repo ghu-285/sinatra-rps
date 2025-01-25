3b9d0fca602844a89b01f53b1eea4d411ee95a6e
ghu-285 <151415214+ghu-285@users.noreply.github.com>
2025-01-25 18:56:17 +0000

    rps

--- app.rb
+++ app.rb
@@ -1,9 +1,51 @@
 require "sinatra"
 require "sinatra/reloader"
 
+moves = ["rock", "paper", "scissors"]
+results = ["won", "lost", "tied"]
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:rules)
+end
+
+get("/rock") do
+  i = rand(0..2)
+  @computer = moves[i]
+  if i == 0
+    @outcome = results[2]
+  elsif i == 1
+    @outcome = results[1]
+  else
+    @outcome = results[0]
+  end
+
+  erb(:rock)
+end
+
+get("/paper") do
+  i = rand(0..2)
+  @computer = moves[i]
+  if i == 0
+    @outcome = results[0]
+  elsif i == 1
+    @outcome = results[2]
+  else
+    @outcome = results[1]
+  end
+
+  erb(:paper)
+end
+
+get("/scissors") do
+  i = rand(0..2)
+  @computer = moves[i]
+  if i == 0
+    @outcome = results[1]
+  elsif i == 1
+    @outcome = results[0]
+  else
+    @outcome = results[2]
+  end
+
+  erb(:scissors)
 end
